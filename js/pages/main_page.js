8a4d7a651631a740b04896342dcc53324c714d93
OrdynaryShark <Andreyd977@gmail.com>
2017-02-14 11:55:38 +0300

    Add Forge

--- js/pages/main_page.js
+++ js/pages/main_page.js
@@ -3,64 +3,73 @@
  */
 
 $(document).ready(function(){
-    var viewer;
+    var oViewer;
     var options = {
         env: 'AutodeskProduction',
-        accessToken: '103WUUOHK8tTK0HHIISElGsNaJIR'
+        accessToken: '2jLGiT3syn7nAUHYo4lNJ7SfZXAd',
+        document : "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE3LTAyLTE0LTA4LTM5LTIxLTJqbGdpdDNzeW43bmF1aHlvNGxuajdzZnp4YWQvUm9ib3RBcm0uZHdmeA"
     };
-    var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXktYnVja2V0L215LWF3ZXNvbWUtZm9yZ2UtZmlsZS5ydnQ=';
-    Autodesk.Viewing.Initializer(options, function onInitialized(){
-        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-    });
+    
+    oViewer =new Autodesk.Viewing.Private.GuiViewer3D ($("#main_viewer") [0], {}) ; // With toolbar
+    Autodesk.Viewing.Initializer (options, function () {
+        oViewer.initialize () ;
+        loadDocument (oViewer, options) ;
+    }) ;
 
-    /**
-     * Autodesk.Viewing.Document.load() success callback.
-     * Proceeds with model initialization.
-     */
-    function onDocumentLoadSuccess(doc) {
+    function loadDocument (viewer, options) {
+        if ( options.document.substring (0, 4) === 'urn:' )
+            options.document =options.document.substring (4) ;
 
-        // A document contains references to 3D and 2D viewables.
-        var viewables = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type':'geometry'}, true);
-        if (viewables.length === 0) {
-            console.error('Document contains no viewables.');
-            return;
-        }
+        Autodesk.Viewing.Document.load ('urn:' + options.document, //options.accessToken,
+            function (doc) { // onLoadCallback
+                oDocument =doc ;
+                // Get all the 3D and 2D views (but keep in separate arrays so we can differentiate in the UI)
+                oViews3D =Autodesk.Viewing.Document.getSubItemsWithProperties (doc.getRootItem (), { 'type': 'geometry', 'role': '3d' }, true) ;
+                oViews2D =Autodesk.Viewing.Document.getSubItemsWithProperties (doc.getRootItem (), { 'type': 'geometry', 'role': '2d' }, true) ;
 
-        // Choose any of the avialble viewables
-        var initialViewable = viewables[0];
-        var svfUrl = doc.getViewablePath(initialViewable);
-        var modelOptions = {
-            sharedPropertyDbPath: doc.getPropertyDbPath()
-        };
+                // Load up first a 3D view by default
+                if ( oViews3D.length > 0 )
+                    oViewer.load (doc.getViewablePath (oViews3D [0])) ;
+                else if ( oViews2D.length > 0 )
+                    oViewer.load (doc.getViewablePath (oViews2D [0])) ;
+                else
+                    return (alert ('ERROR: No 3D or 2D views found in this document!')) ;
 
-        var viewerDiv = document.getElementById('MyViewerDiv');
-        viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
-        viewer.start(svfUrl, modelOptions, onLoadModelSuccess, onLoadModelError);
-    }
+                for ( var i =0 ; i < oViews3D.length ; i++ ) {
+                    var r =$('<div><button id="view_' + i + '" data="' + oViews3D [i].guid + '">' + oViews3D [i].name + '</button></div>') ;
+                    $('#list').append (r) ;
+                    $('#view_' + i).click (function (e) {
+                        e.stopPropagation () ;
+                        var i =parseInt (e.target.id.substring (5)) ;
+                        var guid =oViews3D [i].guid ;
 
-    /**
-     * Autodesk.Viewing.Document.load() failuire callback.
-     */
-    function onDocumentLoadFailure(viewerErrorCode) {
-        console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
-    }
+                        var viewObj =Autodesk.Viewing.Document.getSubItemsWithProperties (doc.getRootItem (), { 'type': 'geometry', 'role': '3d', 'guid': guid }, true) ;
+                        if ( viewObj.length == 0 )
+                            return ;
+                        var path =doc.getViewablePath (viewObj [0]) ;
+                        oViewer.load (path, doc.getPropertyDbPath ()) ;
+                    }) ;
+                }
+                for ( var i =0, j =1000 ; i < oViews2D.length ; j++, i++ ) {
+                    var r =$('<div><button id="view_' + j + '" data="' + oViews2D [i].guid + '">' + oViews2D [i].name + '</button></div>') ;
+                    $('#list').append (r) ;
+                    $('#view_' + j).click (function (e) {
+                        e.stopPropagation () ;
+                        var i =parseInt (e.target.id.substring (5)) - 1000 ;
+                        var guid =oViews2D [i].guid ;
 
-    /**
-     * viewer.loadModel() success callback.
-     * Invoked after the model's SVF has been initially loaded.
-     * It may trigger before any geometry has been downloaded and displayed on-screen.
-     */
-    function onLoadModelSuccess(model) {
-        console.log('onLoadModelSuccess()!');
-        console.log('Validate model loaded: ' + (viewer.model === model));
-        console.log(model);
-    }
+                        var viewObj =Autodesk.Viewing.Document.getSubItemsWithProperties (doc.getRootItem (), { 'type': 'geometry', 'role': '2d', 'guid': guid }, true) ;
+                        if ( viewObj.length == 0 )
+                            return ;
+                        var path =doc.getViewablePath (viewObj [0]) ;
+                        oViewer.load (path, doc.getPropertyDbPath ()) ;
+                    }) ;
+                }
 
-    /**
-     * viewer.loadModel() failure callback.
-     * Invoked when there's an error fetching the SVF file.
-     */
-    function onLoadModelError(viewerErrorCode) {
-        console.error('onLoadModelError() - errorCode:' + viewerErrorCode);
+            },
+            function (errorMsg) { // onErrorCallback
+                alert("Load Error: " + errorMsg) ;
+            }
+        ) ;
     }
 });
